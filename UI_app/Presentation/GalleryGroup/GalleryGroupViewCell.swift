280a45582b5b6d0cfaf192c258c16daf3a4954a7
Plotnik <vyacheslavnazaretsky@gmail.com>
2021-09-09 10:42:08 +0300

    vkGB1.7

---
+++ UI_app/Presentation/GalleryGroup/GalleryGroupViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  GalleryGroupViewCell.swift
+//  UI_app
+//
+//  Created by Вячеслав on 23.08.2021.
+//
+
+import UIKit
+
+final class GalleryGroupViewCell: UICollectionViewCell {
+    @IBOutlet private var Like: Like!
+    var likeTapped: ((Bool) -> Void)?
+    static let identifier: String = "GalleryGroupViewCell"
+
+    @IBOutlet private var ImageGalleryGroup: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        Like.addTarget(self, action: #selector(likeChanged), for: .valueChanged)
+    }
+    
+    func configure(_ galleryGroup: GalleryGroupModel) {
+        
+        ImageGalleryGroup.image = UIImage(named: galleryGroup.imageGalleryGroup)
+        Like.configure(isLike: galleryGroup.isLike, count: galleryGroup.countLike)
+        
+    }
+    @objc private func likeChanged() {
+        likeTapped?(Like.isLike)
+    }
+    
+}
